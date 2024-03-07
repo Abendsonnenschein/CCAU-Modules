2c32c7a85b3ac0838716fae2c5927ac8459ed6a7
Abendsonnenschein <152932131+Abendsonnenschein@users.noreply.github.com>
2024-03-07 13:00:27 -0600

    Add minified output

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-tsc && esbuild --bundle out/index.js --outfile=dist/user.js --banner:js="$(cat meta.txt)"
+tsc
+esbuild --bundle out/index.js --outfile=dist/user.js --banner:js="$(cat meta.txt)"
+esbuild --bundle out/index.js --outfile=dist/mini.js --banner:js="$(cat meta.txt)" --minify
